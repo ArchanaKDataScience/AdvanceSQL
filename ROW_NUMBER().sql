3767c4977d2480659f88377628eff225b6218aac
ArchanaKDataScience <archana.arti1414@gmail.com>
2026-03-21 23:38:05 +0530

    Update ROW_NUMBER().sql

--- ROW_NUMBER().sql
+++ ROW_NUMBER().sql
@@ -1,7 +1,32 @@
 '🔹 Concept: ROW_NUMBER()
 'Used to rank or deduplicate data.
-'Get latest order per customer
+'1.Get latest order per customer
   
   SELECT *,
        ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY order_date DESC) as rn
 FROM orders;
+
+
+  '2.Find the top 2 highest transactions per customer
+SELECT customer_id, order_date, amount
+FROM (
+    SELECT *,
+           ROW_NUMBER() OVER (
+               PARTITION BY customer_id 
+               ORDER BY amount DESC
+           ) AS rn
+    FROM orders
+) t
+WHERE rn <= 2;  
+
+  '3.Find the top 2 unique transaction values (include ties)
+SELECT customer_id, order_date, amount
+FROM (
+    SELECT *,
+           ROW_NUMBER() OVER (
+               PARTITION BY customer_id 
+               ORDER BY amount DESC
+           ) AS rn
+    FROM orders
+) t
+WHERE rn <= 2;  
