53c767227e7240713ff6a68d04bf2c37c0f2e859
ArchanaKDataScience <archana.arti1414@gmail.com>
2026-03-26 02:14:05 +0530

    Update NthHighestSalary.sql
    @N = -1 → @offset = 0 → query runs safely → return NULL  ✅
    @N = 0  → @offset = 0 → query runs safely → return NULL  ✅
    @N = 1  → @offset = 0 → highest salary   → return it    ✅
    @N = 2  → @offset = 1 → 2nd highest      → return it    ✅

--- NthHighestSalary.sql
+++ NthHighestSalary.sql
@@ -1,15 +1,35 @@
 CREATE FUNCTION getNthHighestSalary(@N INT) RETURNS INT AS
 BEGIN
     RETURN (
-        /* Write your T-SQL query statement below. */
-     SELECT ISNULL(
-    (
-        SELECT DISTINCT salary
-        FROM Employee
-        ORDER BY salary DESC
-        OFFSET @N-1 ROW FETCH NEXT 1 ROW ONLY
-    ),
-    NULL
-) AS SecondHighestSalary
+        CASE WHEN @N < 1 THEN NULL
+        ELSE (
+            SELECT ISNULL(
+                (
+                    SELECT DISTINCT salary
+                    FROM Employee
+                    ORDER BY salary DESC
+                    OFFSET abs(@N - 1) ROWS FETCH NEXT 1 ROW ONLY
+                ), NULL)
+        )
+        END
     );
+END
+
+
+or 
+
+CREATE FUNCTION getNthHighestSalary(@N INT) RETURNS INT AS
+BEGIN
+    DECLARE @offset INT = IIF(@N < 1, 0, @N - 1);
+    DECLARE @result INT;
+
+    SELECT @result = ISNULL(
+        (
+            SELECT DISTINCT salary
+            FROM Employee
+            ORDER BY salary DESC
+            OFFSET @offset ROWS FETCH NEXT 1 ROW ONLY
+        ), NULL);
+
+    RETURN IIF(@N < 1, NULL, @result);
 END
